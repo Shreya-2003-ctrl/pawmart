fa038cb9809599cb7826b36c73b633b700a10e25
Shreya Roy <royshreya481@gmail.com>
2026-03-16 02:07:49 +0530

    feat: update image configuration to use remote patterns for Unsplash

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: import('next').NextConfig = {
   images: {
-    domains: ["images.unsplash.com"],
-  },
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "images.unsplash.com",
+    },
+  ],
+},
 };
 
 module.exports = nextConfig;
